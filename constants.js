20055cddd04ac6404b92f0ced8cec7cf08dcd1cf
Mrbrokenlegs <randall@lunchbox.io>
2024-11-10 08:49:14 -0500

    Update constant

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const SAVE_FILE_NAME = 'list.json';
-const TO_DO = "TO_DO";
-const IN_PROGRESS = "IN_PROGRESS";
-const DONE = "DONE";
+const TO_DO = "todo";
+const IN_PROGRESS = "in-progress";
+const DONE = "done";
 
 module.exports = { SAVE_FILE_NAME, TO_DO, IN_PROGRESS, DONE }
